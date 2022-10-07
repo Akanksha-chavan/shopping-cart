adda11d4cf667432d1bad6e316176109c0964a88
Akanksha-chavan <chavan.akanksha6196@gmail.com>
2022-10-07 18:59:06 +0530

    updating directory and category items

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import './categoriesStyles.scss';
+import Directory from './directory/directory.component'
 
 function App() {
+  let categories= [
+    {
+      id:1,
+      title:"Hat",
+      imageUrl:"https://i.ibb.co/cvpntL1/hats.png"
+    },
+    {
+      id:2,
+      title:" Jackets",
+      imageUrl:"https://i.ibb.co/px2tCc3/jackets.png"
+    },
+    {
+      id:3,
+      title:"Sneakers",
+      imageUrl:"https://i.ibb.co/0jqHpnp/sneakers.png"
+    },
+    {
+      id:4,
+      title:"Womens",
+      imageUrl:"https://i.ibb.co/GCCdy8t/womens.png"
+    },
+    {
+      id:5,
+      title:"Mens",
+      imageUrl:"https://i.ibb.co/R70vBrQ/men.png"
+    }
+]
   return (
-    <div className="App">
-      shopping cart
-    </div>
+    <Directory categories={categories}/>
   );
 }
 
